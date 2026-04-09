0400fbe7cc5ec462449d2344c84a4b5a86b83e0d
ashwinam <ashwinam31@gmail.com>
2026-04-09 15:56:00 +0530

    add: add detail template and update view for rendering question

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
 from polls.models import Question
 from django.shortcuts import render
 
@@ -10,7 +10,14 @@ def index(request):
     return render(request, template, context)
 
 def detail(request, question_id):
-    return HttpResponse("You are looking at question %s" % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    
+    template = "polls/detail.html"
+    context = {'question': question}
+    return render(request, template, context)
 
 def result(request, question_id):
     return HttpResponse("You are loking at the result of question %s" % question_id)
