57fdf7cdd0e731ec719bfcaba89e1f4002980698
ashwinam <ashwinam31@gmail.com>
2026-04-10 11:36:30 +0530

    add: add functionality of admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
 from polls.models import Question, Choice
 
+class ChoiceTabularInline(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         ("Question Text", {"fields": ["question_text"]}),
-        ("Date Information", {"fields": ["pub_date"]})
+        ("Date Information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+
+    inlines = [ChoiceTabularInline]
 
 admin.site.register(Question, QuestionAdmin)
 
